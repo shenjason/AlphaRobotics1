1fd18ddeb3e9779ac5796d335c7f36d3a2d5d647
shenjason <83889944+shenjason@users.noreply.github.com>
2023-07-23 14:58:36 -0700

    Manual control fixes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualControl.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "Manual")
+public class ManualControl extends OpMode {
+
+    DcMotor motorLeft;
+    DcMotor motorRight;
+
+
+    @Override
+    public void init(){
+        motorLeft = hardwareMap.get(DcMotor.class, "motorLeft");
+        motorRight = hardwareMap.get(DcMotor.class, "motorRight");
+    }
+
+
+    @Override
+    public void loop(){
+        double powerLeft = gamepad1.left_stick_y;
+        double powerRight = gamepad1.right_stick_y;
+
+        motorLeft.setPower(powerLeft * 0.8);
+        motorRight.setPower(powerRight * 0.8);
+
+
+    }
+}
