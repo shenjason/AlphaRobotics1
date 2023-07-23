5d970208cb377fae7f2b6007f4ceed0f10468d01
ethle25 <ethan.le25@gmail.com>
2023-07-23 15:51:36 -0700

    Update ManualControl.java

--- TeamCode/src/main/java/ManualControl.java
+++ TeamCode/src/main/java/ManualControl.java
@@ -2,7 +2,7 @@
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-
+import com.qualcomm.robotcore.hardware.ColorSensor;
 @TeleOp(name = "Manual")
 public class ManualControl extends OpMode {
 
@@ -18,6 +18,6 @@ public class ManualControl extends OpMode {
 
     @Override
     public void loop(){
-
+        if (color_sensor.blue() == )
     }
 }
